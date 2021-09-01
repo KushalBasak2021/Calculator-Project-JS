bcd47ceb630f4d90a5b839dc6e87b9595d88acfa
KushalBasak2021 <kushalbasak14581@gmail.com>
2021-09-01 13:29:55 +0530

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll(".btn");
 const screen = document.querySelector(".screen");
 const equalButton = document.querySelector(".btn-equal");
 const clearButton = document.querySelector(".btn-clear");
+const backButton = document.querySelector(".btn-back");
 const operatorButtons = document.querySelectorAll(".btn-operation");
 
 buttons.forEach((button) => {
@@ -14,7 +15,7 @@ buttons.forEach((button) => {
 operatorButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const target = e.target.dataset.num;
-    screen.value += ` ${target} `;
+    screen.value += `${target}`;
   });
 });
 
@@ -29,4 +30,11 @@ equalButton.addEventListener("click", (e) => {
 
 clearButton.addEventListener("click", (e) => {
   screen.value = "";
+});
+
+backButton.addEventListener("click", (e) => {
+  if (screen.value !== "") {
+    let newScreenValue = screen.value.substring(0, screen.value.length - 1);
+    screen.value = newScreenValue;
+  }
 });
